164c23fbcf6d8c4bd36abad3edb46ca08a41b3a2
Ronin Cunningham <ronincunningham1@gmail.com>
2019-09-07 00:49:30 -0700

    commit

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>hi</p>
+    <p>bye</p>
+  </div>
+);
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>hi</p>
+    <p>bye</p>
+  </div>
+);
